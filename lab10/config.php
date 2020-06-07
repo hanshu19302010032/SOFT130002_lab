96b4765ebe3d24dff44c0c0b8e5ad21312a5a0ac
hanshu <1726778203@qq.com>
2020-06-07 13:35:57 +0800

    hs's lab10

---
+++ lab10/config.php
@@ -0,0 +1,7 @@
+<?php
+define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'root');
+define('DBPASS', '12345678');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;');
+?>
